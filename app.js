16ef5987e7160f81bb4e451e0c4bd642e8debe3f
Bryson <bryson@brysondev.io>
2022-01-20 20:35:43 -0500

    Modified error handling for specific situations.

--- app.js
+++ app.js
@@ -21,19 +21,37 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  switch (err.status) {
+    case 404:
+      res.status(404).send('Sorry, the resource you are looking for does not exist.');
+      break;
+    case 401:
+      res.status(401).send('Sorry, the resource you are looking for requires authorization.');
+      break;
+    case 403:
+      res.status(403).send('Sorry, the resource you are looking for is inaccessable at this time to you.');
+      break;
+    case 408:
+      res.status(408).send('Sorry, the request timed out. Please try again.');
+      break;
+    case 410:
+      res.status(410).send('Gone.');
+      break;
+    default:
+      res.status(500).send('Sorry, something appears wrong with the server. Kindly contact the system admin to correct this issue or wait a little while.');
+      break;
+  }
+
 });
 
 module.exports = app;
